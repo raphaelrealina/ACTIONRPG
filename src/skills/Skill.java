629fd2397e829b51fcba50563587546114806841
SilentFire494 <Silentfire494@gmail.com>
2024-02-06 13:56:33 -0600

    Moved Skill to skills package

---
+++ src/skills/Skill.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Skill {
+    int value;
+    List<Skill> children;
+
+    public Skill(int value) {
+        this.value = value;
+        this.children = new ArrayList<Skill>();
+    }
+}
